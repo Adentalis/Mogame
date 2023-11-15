5857396db98d7d74fe8febca93e7ceb3721a11c4
Adentalis <daniel.n96@t-online.de>
2023-11-15 23:19:53 +0100

    start layouting page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <div id="content">
+  <div id="content-left">
     <h1>Mogame</h1>
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+    <PlanetLink />
+  </div>
+  <div id="content-middle">
     <button @click="test">Test</button>
     {{ persons }}
   </div>
+  <div id="content-bottom"></div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-
+import PlanetLink from './components/PlanetLink.vue';
 const persons = ref([]);
 
 onMounted(async () => {
@@ -25,8 +46,26 @@ const test = (): number => {
 </script>
 
 <style scoped>
-#content {
-  color: blue;
-  background-color: #242424;
+#content-left {
+  border: 3px green solid;
+  flex-basis: 25%;
+  overflow-y: auto;
+  height: calc(100vh - 50px);
+}
+
+#content-middle {
+  border: 3px orange solid;
+  flex-basis: 75%;
+  overflow-y: auto;
+  height: calc(100vh - 50px);
+}
+
+#content-bottom {
+  border: 3px blue solid;
+  width: 100%;
+  height: 40px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
 }
 </style>
---
+++ frontend/src/components/PlanetLink.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="planet-link">Planet 1</div>
+</template>
+
+<style scoped>
+.planet-link {
+  border: 2px solid purple;
+  min-height: 100px;
+}
+</style>
