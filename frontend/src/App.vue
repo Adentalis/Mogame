d5d1ebe1c0ef06c948b36a5fb325d42e4965817b
Adentalis <daniel.n96@t-online.de>
2023-11-15 18:17:33 +0100

    show persons in frontend onMounted

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,32 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="content">
+    <h1>Mogame</h1>
+    <button @click="test">Test</button>
+    {{ persons }}
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const persons = ref([]);
+
+onMounted(async () => {
+  const req = await fetch('http://localhost:3000/persons');
+  const response = await req.json();
+  persons.value = response;
+  console.log(response);
+});
+
+const test = (): number => {
+  console.log('the-');
+  return 3;
+};
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#content {
+  color: blue;
+  background-color: #242424;
 }
 </style>
